feefc642194f528019212fb8f716c9e36b4e26cf
Daniela Cohen <danielacohen@Danielas-MacBook-Pro.local>
2023-09-21 10:53:12 +0200

    addeed dev

---
+++ main.js
@@ -0,0 +1,24 @@
+const face = document.querySelector('.js-face');
+
+
+
+
+
+function handleClick() {
+
+    face.innerHTML = ';)';
+
+};
+
+face.addEventListener('click', handleClick);
+
+
+// function mouseover () {
+ 
+//     face.innerHTML = ':)';
+// };
+
+// face.addEventListener('mouseover', mouseover);
+
+
+
